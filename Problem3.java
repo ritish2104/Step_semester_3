5d6b40816c1bf049f1d5ff505258eb01182e65ba
Your Name <your.email@example.com>
2026-08-27 23:20:40 +0530

    level 2 problem 3

---
+++ Problem3.java
@@ -0,0 +1,16 @@
+public class Problem3 {
+    public static void printSkippingMultiplesOfThree() {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 3 == 0) {
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Runs automatically without needing manual numerical inputs
+        printSkippingMultiplesOfThree();
+    }
+}
